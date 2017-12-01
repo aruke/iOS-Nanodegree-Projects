69fec17250c85f463a6156f8d582fa4b46ad91f5
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2017-12-01 16:30:56 +0530

    PitchPerfect - Add new ViewController

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func startRecording(_ sender: Any) {
+        recordButton.isEnabled = false
+        stopButton.isEnabled = true
+        recordLabel.text = "Recording.."
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordButton.isEnabled = true
+        stopButton.isEnabled = false
+        recordLabel.text = "Tap To Record"
+    }
+    
 }
 
