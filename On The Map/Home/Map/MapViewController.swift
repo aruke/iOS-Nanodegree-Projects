c8dbdc522975d8d8c9f4257327c657dbf984e96f
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2018-03-19 00:07:53 +0530

    OnTheMap - feat: adds tab controller in scene

---
+++ On The Map/Home/Map/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  On The Map
+//
+//  Created by Rajanikant Deshmukh on 18/03/18.
+//  Copyright © 2018 Rajanikant Deshmukh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapViewController: UIViewController {
+    
+}
---
+++ On The Map/Home/Table/TableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewController.swift
+//  On The Map
+//
+//  Created by Rajanikant Deshmukh on 18/03/18.
+//  Copyright © 2018 Rajanikant Deshmukh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewController: UIViewController {
+    
+}
