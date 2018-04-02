2b750e874457e043e1f6def6f549b34cc2996746
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2018-04-02 17:57:26 +0530

    JSON-Feed-Reader - Update: Adds shallow navigation

--- JSON Feed Reader/UI/FeedDetailsViewController.swift
+++ JSON Feed Reader/UI/FeedDetailsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FeedDetailsViewController.swift
 //  JSON Feed Reader
 //
 //  Created by Rajanikant Deshmukh on 02/04/18.
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeedDetailsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
